4d5c6f286b8849924423b440f408c8e52dc6b477
cproffit <cproffit730@gmail.com>
2024-01-13 19:26:27 -0500

    Game Manager/Pause menu functionality added

---
+++ SFShooter_Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField] GameObject activeMenu;
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject winMenu;
+    [SerializeField] GameObject loseMenu;
+
+    public GameObject player;
+
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel") && activeMenu == null)
+        {
+            PausedState();
+            activeMenu = pauseMenu;
+            activeMenu.SetActive(isPaused);
+        }
+    }
+
+    public void PausedState()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void UnpausedState()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
+}
