fde13e664210ddd87d02007f24891a0e6d7b98c4
Dpair26 <116693274+Dpair26@users.noreply.github.com>
2024-01-17 12:30:14 -0600

    Pause Menu

---
+++ SFShooter_Game/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        GameManager.instance.UnpausedState();
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.instance.UnpausedState();
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+}
+
+
