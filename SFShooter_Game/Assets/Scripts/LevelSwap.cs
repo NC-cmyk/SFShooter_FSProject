54a5a281d94f7fa445d19e6f5d80ad21108055f5
NC-cmyk <123971573+NC-cmyk@users.noreply.github.com>
2024-02-15 10:17:46 -0600

    Level transitioning
    - level swapping is now possible
    - however there's no current way for the player to save its stats if they were to ever be altered

---
+++ SFShooter_Game/Assets/Scripts/LevelSwap.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwap : MonoBehaviour
+{
+    [Header("--- Level Chooser ---")]
+    [Range(0, 4)] [SerializeField] int levelToLoad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(loadScene());
+        }
+    }
+
+    IEnumerator loadScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(GameManager.instance.sceneNames[levelToLoad]);
+
+        while(!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        GameManager.instance.playerScript.respawn();
+        // However doing this would require the player needing a way to retain their upgrades
+        // If upgrades are still getting implemented
+    }
+}
