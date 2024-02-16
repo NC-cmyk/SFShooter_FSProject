b98033337adc0c787b7f6d447e9e1327f3a4cf43
Dpair26 <116693274+Dpair26@users.noreply.github.com>
2024-02-16 12:41:12 -0600

    Volume Settings

---
+++ SFShooter_Game/Assets/Scripts/VolumeSettings.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider soundSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = soundSlider.value;
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void LoadVolume()
+    {
+        soundSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+}
+        
+
+
+
+
+
+
+
+              
