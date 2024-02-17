bc7eaa8ebbe1eae289da484d68525e891db2c005
Dpair26 <116693274+Dpair26@users.noreply.github.com>
2024-02-16 19:56:42 -0600

    Volume settings

--- SFShooter_Game/Assets/Scripts/VolumeSettings.cs
+++ SFShooter_Game/Assets/Scripts/VolumeSettings.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
-
+using TMPro;
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] private Slider soundSlider;
-    [SerializeField] private Slider SFXSlider;
+    [SerializeField] private Slider soundSlider = null;
+    [SerializeField] private Slider SFXSlider = null;
+    [SerializeField] private GameObject confirmationPrompt = null;
+    [SerializeField] private TMP_Text volumeTextvalue;
+    [SerializeField] private TMP_Text volumeSFXTextvalue;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+
+    private bool isGameObjectActive = false;
+
 
     public void Start()
     {
@@ -21,36 +29,66 @@ public class VolumeSettings : MonoBehaviour
             SetMusicVolume();
             SetSFXVolume();
         }
+        if (gameObject.activeSelf)
+        {
+            isGameObjectActive = true;
+            StartCoroutine(CofirmationLoadBox());
+        }
+        else
+        {
+            isGameObjectActive = false;
+        }
     }
 
     public void SetMusicVolume()
     {
         float volume = soundSlider.value;
-        audioMixer.SetFloat("SFX", Mathf.Log10(volume)*20);
-        PlayerPrefs.SetFloat("SFXVolume", volume);
+        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        AudioListener.volume = volume;
+        volumeTextvalue.text = volume.ToString("0.0");
     }
 
     public void SetSFXVolume()
     {
         float volume = SFXSlider.value;
-        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("musicVolume", volume);
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        AudioListener.volume = volume;
+        volumeSFXTextvalue.text = volume.ToString("0.0");
     }
 
     public void LoadVolume()
     {
         soundSlider.value = PlayerPrefs.GetFloat("musicVolume");
         SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        PlayerPrefs.SetFloat("musicVolume", AudioListener.volume);
+        PlayerPrefs.SetFloat("SFXVolume", AudioListener.volume);
+        StartCoroutine(CofirmationLoadBox());
         SetMusicVolume();
         SetSFXVolume();
     }
-}
-        
 
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            soundSlider.value= defaultVolume;
+            SFXSlider.value= defaultVolume;
+            volumeTextvalue.text = defaultVolume.ToString("0.0");
+            LoadVolume();
+        }
+    }
 
+    public IEnumerator CofirmationLoadBox()
+    {
+        if (isGameObjectActive)
+        {
+            confirmationPrompt.SetActive(true);
+            yield return new WaitForSeconds(2);
+            confirmationPrompt.SetActive(false);
+        }
 
-
-
-
-
-              
+    }
+}       
