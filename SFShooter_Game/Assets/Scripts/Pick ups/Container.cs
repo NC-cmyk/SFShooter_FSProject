0fb962e8fa0a3e29722ccb526d4bf7413ea6096e
NC-cmyk <123971573+NC-cmyk@users.noreply.github.com>
2024-02-20 15:10:00 -0600

    Beginning work on container visuals

---
+++ SFShooter_Game/Assets/Scripts/Pick ups/Container.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    [Header("--- Container Components ---")]
+    [SerializeField] GameObject closedEgg;
+    [SerializeField] GameObject openEgg;
+    [SerializeField] ParticleSystem eggBurst;
+
+    public void openContainer()
+    {
+        closedEgg.SetActive(false);
+        openEgg.SetActive(true);
+        eggBurst.Play();
+    }
+}
