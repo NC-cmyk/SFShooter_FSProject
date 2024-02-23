eae2e42a32e570b0af6790b709a85fe4ad0a1b33
NC-cmyk <123971573+NC-cmyk@users.noreply.github.com>
2024-02-23 13:04:34 -0600

    Scrap visual updates
    - updated scrap material to make it seem less flat
    - added code to scrap script Update() to make it bob up and down and rotate

--- SFShooter_Game/Assets/Scripts/Pick ups/Scrap.cs
+++ SFShooter_Game/Assets/Scripts/Pick ups/Scrap.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class Scrap : MonoBehaviour
 {
+    bool goUp, isSwitching;
+    float ogY, offsetY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
         // add to the scrap needed 
         ScrapTracker.instance.requiredScrapCount += 1;
+
+        // for animation
+        goUp = true;
+        ogY = transform.position.y;
+        offsetY = transform.position.y + 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // make collectable when all enemies are dead
+        // rotate
+        transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * 50);
 
+        // bob up and down
+        if (goUp)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, offsetY, Time.deltaTime * 0.5f), transform.position.z);
+        }
+        else if(!goUp)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, ogY, Time.deltaTime * 0.5f), transform.position.z);
+        }
+
+        // switch between going up and going down
+        if (!isSwitching)
+        {
+            StartCoroutine(floatSwitch());
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player")){
-            Debug.Log("Scrap collected");
             ScrapTracker.instance.CollectScrap();
             Destroy(gameObject);
         }
     }
 
+
+    IEnumerator floatSwitch()
+    {
+        isSwitching = true;
+
+        yield return new WaitForSeconds(1);
+        goUp = !goUp;
+
+        isSwitching = false;
+    }
 }
